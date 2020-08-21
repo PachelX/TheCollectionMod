eeb6e6819ddca04eda2aa25b196dfdc345459442
PachelX <70034187+PachelX@users.noreply.github.com>
2020-08-21 21:53:01 +0200

    Add files via upload

--- Buffs/ShurikenjutsuBuff.cs
+++ Buffs/ShurikenjutsuBuff.cs
@@ -23,7 +23,7 @@ namespace TheCollectionMod.Buffs
                     + "\n+60% probabilidad de crítico."
                     + "\n+60% velocidad de lanzamiento a las armas arrojadizas.");
             }
-            else if (NPC.downedBoss3)
+            /*else if (NPC.downedBoss3)
             {
                 DisplayName.SetDefault("Shuriken Jutsu v4");
                 DisplayName.AddTranslation(GameCulture.Spanish, "Arte Ninja - Shuriken v4");
@@ -55,104 +55,52 @@ namespace TheCollectionMod.Buffs
                 Description.AddTranslation(GameCulture.Spanish, "+40% de daño a las armas arrojadizas."
                     + "\n+40% probabilidad de crítico."
                     + "\n+40% velocidad de lanzamiento a las armas arrojadizas.");
-            }
+            }*/
             else
             {
                 DisplayName.SetDefault("Shuriken Jutsu");
                 DisplayName.AddTranslation(GameCulture.Spanish, "Arte Ninja - Shuriken");
-                Description.SetDefault(" +30% Increases throwing damage"
-                    + "\n+30% increased throwing critical strike chance."
-                    + "\n+30% increased throwing speed.");
-                Description.AddTranslation(GameCulture.Spanish, "+30% de daño a las armas arrojadizas."
-                    + "\n+30% probabilidad de crítico."
-                    + "\n+30% velocidad de lanzamiento a las armas arrojadizas.");
+                Description.SetDefault("Increases throwing damage, speed and critical strike chance.");
+                Description.AddTranslation(GameCulture.Spanish, "Aumento de daño, probabilidad de crítico y velocidad de lanzamiento a las armas arrojadizas.");
             }
         }
 
         public override void Update(Player player, ref int buffIndex)
         {
-            if (NPC.downedMoonlord)
+            if (Main.hardMode)
             {
-                player.thrownVelocity += 20f;
-                player.thrownDamage += 20f;
-                player.thrownCrit += 2000;
-            }
-            else if (NPC.downedTowers)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (NPC.downedAncientCultist)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (NPC.downedFishron)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (DD2Event.DownedInvasionT1)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
+                player.thrownVelocity += 3f;
+                player.thrownDamage += 3f;
+                player.thrownCrit += 300;
             }
             else if (NPC.downedBoss3)
             {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (NPC.downedBoss3)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (NPC.downedBoss3)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (NPC.downedBoss3)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 600;
-            }
-            else if (NPC.down)
-            {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
-                player.thrownCrit += 200;
+                player.thrownVelocity += 2.5f;
+                player.thrownDamage += 2.5f;
+                player.thrownCrit += 250;
             }
             else if (NPC.downedQueenBee)
             {
-                player.thrownVelocity += 6f;
-                player.thrownDamage += 6f;
+                player.thrownVelocity += 2f;
+                player.thrownDamage += 2f;
                 player.thrownCrit += 200;
             }
             else if (NPC.downedBoss2)
             {
-                player.thrownVelocity += 0.5f;
-                player.thrownDamage += 0.5f;
+                player.thrownVelocity += 1.5f;
+                player.thrownDamage += 1.5f;
                 player.thrownCrit += 150;
             }
             else if (NPC.downedBoss1)
             {
-                player.thrownVelocity += 0.4f;
-                player.thrownDamage += 0.4f;
+                player.thrownVelocity += 1f;
+                player.thrownDamage += 1f;
                 player.thrownCrit += 100;
             }
             else if (NPC.downedSlimeKing)
             {
-                player.thrownVelocity += 1f;
-                player.thrownDamage += 1f;
+                player.thrownVelocity += 0.5f;
+                player.thrownDamage += 0.5f;
                 player.thrownCrit += 50;
             }
             else 
